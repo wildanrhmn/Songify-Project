331463be5320956d3d1c90f875ffcec107debd6c
Wildan <wildannurrahman30@gmail.com>
2022-11-28 23:00:14 +0700

    Fetch, custom hooks, loader, error
    -Making a fetch to the Shazam API using rapidAPI.
    
    -Implementing Redux createApi
    
    -Error and Loader Jsx Files

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,33 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'c6031d9ab7msh81f61c37fc10ca3p1e95d3jsnbe5d32c3759a',
+      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'c6031d9ab7msh81f61c37fc10ca3p1e95d3jsnbe5d32c3759a');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({query: () => 'charts/world'}),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
